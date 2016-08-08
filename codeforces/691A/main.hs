43fa1f95616093d4e811081b45abf9a66f6c76b8
codeonwort <codeonwort@clug-beta.(none)>
2016-08-08 15:34:35 +0900

    delete duplicates

--- codeforces/691A/main.hs
+++
@@ -1,9 +0,0 @@
-getInt = read `fmap` getLine :: IO Int
-getInts = (map read . words) `fmap` getLine :: IO [Int]
-
-main = do
-	n <- getInt
-	xs <- getInts
-	if n == 1
-	then if xs == [1] then putStrLn "YES" else putStrLn "NO"
-	else if (filter (==0) xs) == [0] then putStrLn "YES" else putStrLn "NO"
--- codeforces/697A/main.hs
+++
@@ -1,9 +0,0 @@
-main = do
-	[t,s,x] <- (map read . words) `fmap` getLine :: IO [Int]
-	if x < t
-	then putStrLn "NO"
-	else if (x-t) `mod` s == 0
-	then putStrLn "YES"
-	else if (x-t-1) `mod` s == 0 && x /= t + 1
-	then putStrLn "YES"
-	else putStrLn "NO"
